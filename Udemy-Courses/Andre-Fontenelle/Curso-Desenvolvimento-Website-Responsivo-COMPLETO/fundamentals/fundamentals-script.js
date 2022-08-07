4c72a71d6ab9568ff310038c91dc4bcca522395f
Yuji Tanaka <yujitanaka.developer@gmail.com>
2022-08-07 11:33:46 -0300

    Update fundamentals

--- Udemy-Courses/Andre-Fontenelle/Curso-Desenvolvimento-Website-Responsivo-COMPLETO/fundamentals/fundamentals-script.js
+++ Udemy-Courses/Andre-Fontenelle/Curso-Desenvolvimento-Website-Responsivo-COMPLETO/fundamentals/fundamentals-script.js
@@ -1,16 +1,16 @@
 // document é o documento/página HTML que estamos trabalhando.
 // querySelector é a função que busca/pega o que quisermos dentro da nossa pasta.
-var meuBotao = document.querySelector("#botao");
-function mostrarAviso(){
-    alert("Hey! You clicked me!");
-    }    
-meuBotao.addEventListener('click', mostrarAviso);
+var myButton = document.querySelector("#button");
+function showWarning() {
+   alert("Hey! You clicked me!");
+}
+myButton.addEventListener("click", showWarning);
 // Indentificando elementos da tag <ul> do arquivo HTML
-var ingrediente = document.querySelector("ul").children[2].innerHTML;
-// Teste interno para identificar o elemento dentro de <ul> 
-console.log(ingrediente);
+var ingredient = document.querySelector("ul").children[2].innerHTML;
+// Teste interno para identificar o elemento dentro de <ul>
+console.log(ingredient);
 // Acrescentar lista não ordenada no documento HTML através do JS (apenas para testes - não recomendado fazer isso).
 var lista = document.querySelector("ul");
-var novo_item = document.createElement("li");
-lista.appendChild(novo_item);
+var new_item = document.createElement("li");
+lista.appendChild(new_item);
 document.querySelector("ul").children[3].innerHTML = "Healthier Proteins!";
